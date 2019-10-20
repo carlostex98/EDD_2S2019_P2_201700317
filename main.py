04c001b0d98cbb37315db2c8572e40ca59413582
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2019-10-20 14:29:22 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import hashlib
-
+import os
 #temporal de clase
 class tmp_lst():
 	def __init__(self):
@@ -39,8 +39,8 @@ class m_lst():
 		self.ultimo = None
 		self.tamanio = 0
 
-	def agregar(self, index, tm_stamp, c_class, p_hash, a_hash, arbol_i):
-		nuevo_b = lst(index, tm_stamp, c_class, p_hash, a_hash, arbol_i)
+	def agregar(self, tm_stamp, c_class, p_hash, a_hash, arbol_i):
+		nuevo_b = lst(self.tamanio, tm_stamp, c_class, p_hash, a_hash, arbol_i)
 		if self.primero is None:
 			self.primero=nuevo_b
 			self.ultimo=nuevo_b
@@ -49,29 +49,243 @@ class m_lst():
 			nuevo_b.anterior=self.ultimo
 			self.ultimo.siguiente=nuevo_b
 			self.ultimo=nuevo_b
+			self.tamanio=self.tamanio+1
 
 
-class ar_avl():
+class ar_avl(object):
 	def __init__(self, carnet, nombre):
-		self.izquierda = None
-		self.derecha = None
+		self.left = None
+		self.right = None
 		self.carnet = carnet
 		self.nombre = nombre
+		self.hei = 1
 
-class m_arbol():
-	def insertar(self,nombre, carnet):
+class m_arbol(object):
+	def __init__ (self):
+		self.tmp_rep= ""
+
+	def insertar(self, carnet, nombre, raiz):
+		if not raiz:
+			return ar_avl(carnet, nombre)
+		elif carnet < raiz.carnet:
+			raiz.left = self.insertar(carnet, nombre, raiz.left)
+		else :
+			raiz.right = self.insertar(carnet, nombre, raiz.right)
+
+		#2
+		raiz.hei = 1 + max(self.get_h(raiz.left), self.get_h(raiz.right))
+
+		#3
+		balance = self.get_bal(raiz)
+
+		#4-balanceo
+		if balance > 1 and carnet < raiz.left.carnet:
+			return self.rot_der(raiz)
+
+		if balance < -1	and carnet > raiz.right.carnet:
+			return self.rot_izq(raiz)
+		
+		if balance > 1 and carnet > raiz.left.carnet:
+			raiz.left = self.rot_izq(raiz.left)
+			return self.rot_der(raiz)
+		
+		if balance < -1 and carnet < raiz.right.carnet:
+			raiz.right = self.rot_der(raiz.right)
+			return self.rot_izq(raiz)
+		
+		return raiz
+
+	def get_h(self, t_raiz):
+		if not t_raiz:
+			return 0
+		else:
+			return t_raiz.hei
+	
+	def get_bal(self, t_raiz):
+		if not t_raiz:
+			return 0
+		return self.get_h(t_raiz.left) - self.get_h(t_raiz.right)
+
+	def rot_der(self, w):
+		y = w.left
+		t3 = y.right
+
+		y.right = w
+		w.left = t3
+
+		w.hei = 1 + max(self.get_h(w.left), self.get_h(w.right))
+		y.hei = 1 + max(self.get_h(y.left), self.get_h(y.right))
+
+		return y
+
+	def rot_izq(self, w):
+		y=w.right
+		t2=y.left
+
+		y.left = w
+		w.right=t2
+
+		w.hei = 1 + max(self.get_h(w.left), self.get_h(w.right))
+		y.hei = 1 + max(self.get_h(y.left), self.get_h(y.right))
+
+		return y
+	
+	def pre_ord(self, raiz):
+		if not raiz:
+			return
+		print ( str(raiz.carnet) +" ,"+ str(raiz.nombre))
+		self.pre_ord(raiz.left)
+		self.pre_ord(raiz.right)
+
+	def arbol_dat(self, raiz):
+		if not raiz:
+			return
+		ss="Carnet :"+str(raiz.carnet)+  "\\n Nombre: "+str(raiz.nombre) +" \\n Altura:"+str(self.get_h(raiz))+" \\n FE:"+str(self.get_bal(raiz))
+		self.tmp_rep += "cc"+str(raiz.carnet) + \
+			"[shape=record, label=\"<C0>|"+ss+" |<C1>  "+"\"]; \n"
+		#print(str(raiz.carnet) + " ," + str(raiz.nombre))
+		self.arbol_dat(raiz.left)
+		self.arbol_dat(raiz.right)
+	
+	def arbol_rel(self, raiz):
+		if not raiz:
+			return
+		if  raiz.left != None or raiz.right != None:
+			if raiz.left != None:
+				#aux = raiz.right
+				self.tmp_rep += "cc"+str(raiz.carnet) + ":C1  -> cc" + str(raiz.left.carnet) + "; \n"
+			if raiz.right != None:
+				#aux1 = raiz.left
+				self.tmp_rep += "cc"+str(raiz.carnet) + ":C0  -> cc" + str(raiz.right.carnet) + "; \n"
+		#print(str(raiz.carnet) + " ," + str(raiz.nombre))
+		self.arbol_rel(raiz.left)
+		self.arbol_rel(raiz.right)
+
+
+	def prt_avl_rep_nor(self, raiz):
+		f=open("arb_avl.dot","w")
+		f.write("digraph { \n")
+		self.arbol_dat(raiz)
+		f.write(self.tmp_rep)
+		self.tmp_rep=""
+
+		self.arbol_rel(raiz)
+		f.write(self.tmp_rep)
+		self.tmp_rep=""
+		f.write("} ")
+		f.close()
+		os.system("dot -Tjpg arb_avl.dot -o imagen_avl.jpg")
+		
+		os.system("xdg-open imagen_avl.jpg")
+
+	def reporte_preord(self, raiz):
+		if not raiz:
+			return
+		l_tmp.agregar(raiz.carnet, raiz.nombre)
+		self.reporte_preord(raiz.left)
+		self.reporte_preord(raiz.right)
+
+	def reporte_inord(self, raiz):
+		if not raiz:
+			return
+		self.reporte_inord(raiz.left)
+		l_tmp.agregar(raiz.carnet, raiz.nombre)
+		self.reporte_inord(raiz.right)
+
+	def reporte_postord(self, raiz):
+		if not raiz:
+			return
+		self.reporte_postord(raiz.left)
+		self.reporte_postord(raiz.right)
+		l_tmp.agregar(raiz.carnet, raiz.nombre)
+		
+		
+
+class l_temporal():
+	def __init__(self, carnet, nombre):
+		self.carnet = carnet
+		self.nombre = nombre
+		self.siguiente = None
+
+class m_temporal():
+	def __init__(self):
+		self.primero = None
+		self.ultimo = None
+	
+	def truncar(self):
+		self.primero = None
+		self.ultimo = None
+	
+	def agregar(self, carnet, nombre):
+		nuevo = l_temporal(carnet, nombre)
+		if(self.primero is None):
+			self.primero = nuevo
+			self.ultimo = nuevo
+		else:
+			self.ultimo.siguiente=nuevo
+			self.ultimo=nuevo
+	def genera_reporte_gnd(self, raiz, tipo):
+		if tipo == 0:
+			arbolito.reporte_preord(raiz)
+		if tipo == 1:
+			arbolito.reporte_postord(raiz)
+		if tipo == 2:
+			arbolito.reporte_inord(raiz)
+
+		
+		self.tmp_rep = ""
+		f = open("arb_inord.dot", "w")
+		f.write("digraph { \n node[shape=box]; \n rankdir=LR; \n")
+		actual = self.primero
+		while True:
+			s="cc"+str(actual.carnet)+"[label=\" "+str(actual.carnet)+" \\n  "+str(actual.nombre)+" \" ]; \n"
+			f.write(s)
+			if not actual.siguiente is None:
+				s="cc"+str(actual.carnet)+" -> cc"+str(actual.siguiente.carnet)+" ;\n"
+				f.write(s)
+			actual=actual.siguiente
+			if actual is None:
+				break
+
+		f.write("} \n")
+		f.close()
+		os.system("dot -Tjpg arb_inord.dot -o imagen_avl1.jpg")
+
+		os.system("xdg-open imagen_avl1.jpg")
+
+
+			
+
 		
 
 
 #inicializacion
 tmp_a = cab_avl()
 tmp_l = tmp_lst()
-
+l_tmp = m_temporal()
+arbolito = m_arbol()
+#0 preord
+#1 postord
+#2 inord
 
 
 
 def main():
-	print("hello")
+	print("hello \n")
+
+	rr = None
+	rr = arbolito.insertar(10, "michael 1", rr)
+	rr = arbolito.insertar(20, "michael 2", rr)
+	rr = arbolito.insertar(30, "michael 3", rr)
+	rr = arbolito.insertar(40, "michael 4", rr)
+	rr = arbolito.insertar(50, "michael 5", rr)
+	rr = arbolito.insertar(25, "michael 25", rr)
+	#print(rr.carnet)
+	#arbolito.pre_ord(rr)
+	arbolito.prt_avl_rep_nor(rr)
+	#l_tmp.genera_reporte_gnd(rr,0)
+	
+
 
 
 main()
